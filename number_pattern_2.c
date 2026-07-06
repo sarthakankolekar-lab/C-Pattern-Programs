29a4536a2708d8e036b255bc356af8960d4c0069
sarthakankolekar-lab <sarthakankolekar@gmail.com>
2026-07-06 08:16:57 +0530

    number_pattern_2

---
+++ number_pattern_2.c
@@ -0,0 +1,30 @@
+//Code
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    scanf("%d", &n);
+    printf("\n");
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*Output
+
+5
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+*/
